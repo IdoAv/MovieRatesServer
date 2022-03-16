a9f91033aa8878f28148445fef184898d057ba15
idoaviv19031@hotmail.com <idoaviv19031@hotmail.com>
2022-03-16 14:47:27 +0200

    v1

--- MovieRatesBL/ModelsBl/MovieRatesDBContext.cs
+++ MovieRatesBL/ModelsBl/MovieRatesDBContext.cs
@@ -8,5 +8,22 @@ namespace MovieRatesBL.Models
 {
     public partial class MovieRatesDBContext
     {
+        public User GetUser(string userName,string pass)
+        {
+            return this.Users.Where(u => u.UserName == userName && u.Pass == pass).FirstOrDefault();
+        }
+        public void Register(User u)
+        {
+            this.Users.Add(u);
+            this.SaveChanges();
+        }
+        public bool EmailExist(string email)
+        {
+            return this.Users.Count(u => u.Email == email) > 0;
+        }
+        public bool UserNameExist(string userName)
+        {
+            return this.Users.Count(u => u.UserName == userName) > 0;
+        }
     }
 }
--- MovieRatesServer/Controllers/MovieRatesController.cs
+++ MovieRatesServer/Controllers/MovieRatesController.cs
@@ -17,6 +17,75 @@ namespace MovieRatesServer.Controllers
         {
             this.context = context;
         }
+        [Route("Login")]
+        [HttpGet]
+        public User Login([FromQuery] string userName,[FromQuery] string pass)
+        {
+            User user = context.GetUser(userName, pass);
+
+            //Check user name and password
+            if (user != null)
+            {
+                HttpContext.Session.SetObject("theUser", user);
+
+                Response.StatusCode = (int)System.Net.HttpStatusCode.OK;
+
+                //Important! Due to the Lazy Loading, the user will be returned with all of its contects!!
+                return user;
+            }
+            else
+            {
+
+                Response.StatusCode = (int)System.Net.HttpStatusCode.Forbidden;
+                return null;
+            }
+        }
+        [Route("Register")]
+        [HttpPost]
+        public bool Register([FromBody] User u)
+        {
+            if(HttpContext.Session.GetObject<User>("theUser") != null)
+            {
+                Response.StatusCode = (int)System.Net.HttpStatusCode.Forbidden;
+                return false;
+            }
+            try
+            {
+                context.Register(u);
+            }
+            catch
+            {
+                Response.StatusCode = (int)System.Net.HttpStatusCode.Forbidden;
+                return false;
+            }
+            HttpContext.Session.SetObject("theUser", u);
+            Response.StatusCode = (int)System.Net.HttpStatusCode.OK;
+            return true;
+        }
+        [Route("EmailExist")]
+        [HttpGet]
+        public bool EmailExist([FromQuery] string email)
+        {
+            if (!context.EmailExist(email))
+            {
+                Response.StatusCode = (int)System.Net.HttpStatusCode.OK;
+                return false;
+            }
+            Response.StatusCode = (int)System.Net.HttpStatusCode.OK;
+            return true;
+        }
+        [Route("UserNameExist")]
+        [HttpGet]
+        public bool UserNameExist([FromQuery] string userName)
+        {
+            if (!context.UserNameExist(userName))
+            {
+                Response.StatusCode = (int)System.Net.HttpStatusCode.OK;
+                return false;
+            }
+            Response.StatusCode = (int)System.Net.HttpStatusCode.OK;
+            return true;
+        }
         #endregion
     }
 }
